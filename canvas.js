2324a28d769cf875488f4d88d3d8e994770b1699
aidandkim <aidandkim@gmail.com>
2018-01-21 11:04:52 -0800

    created canvas

--- canvas.js
+++ canvas.js
@@ -5,9 +5,8 @@ window.onload = init;
 function init(){
     canvas = document.getElementById("my-canvas");
     ctx = canvas.getContext('2d');
- drawRectangle();
-    
-}
-function drawRectangle() {
-    ctx.fillRect(0,0,100,100);
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0,0,140,200); // background
+    ctx.fillStyle = "black";
+    ctx.fillRect(30, 10, 80, 10);
 }
