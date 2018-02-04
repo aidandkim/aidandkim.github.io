8d09a868ace1c2313221f2fb541ddc2e3a30e2a6
aidandkim <aidandkim@gmail.com>
2018-02-04 11:41:06 -0800

    i made java

--- canvas.js
+++ canvas.js
@@ -3,7 +3,7 @@ var ctx;
 window.onload = init;
 
 function init(){
-    canvas = document.getElementById("my-canvas");
+    // canvas = document.getElementById("my-canvas");
     ctx = canvas.getContext('2d');
     ctx.fillStyle = "grey";
     ctx.fillRect(0,0,140,200); // background
