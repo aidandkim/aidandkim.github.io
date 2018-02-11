5b1798c7fa7d4a34c4c2da871dd3bed2ad3da687
aidandkim <aidandkim@gmail.com>
2018-02-11 09:49:55 -0800

    js

--- canvas2.js
+++ canvas2.js
@@ -1,78 +1,19 @@
-ctx.fillStyle = "grey";
-ctx.fillRect(0,0,140,200); // background
-ctx.fillStyle = "black";
-ctx.fillRect(30, 10, 80, 10);
-ctx.fillRect(20, 20, 10, 10);
-ctx.fillRect(110, 20, 10, 10);
-ctx.fillRect(10, 30, 10, 60);
-ctx.fillRect(30, 50, 20, 10);
-ctx.fillRect(80, 50, 20, 10);
-ctx.fillRect(120, 30, 10, 60);
-ctx.fillRect(10, 90, 20, 10);
-ctx.fillRect(110, 90, 20, 10);
-ctx.fillRect(30, 100, 80, 10);
-ctx.fillRect(20, 110, 10, 10);
-ctx.fillRect(110, 110, 10, 10);
-ctx.fillRect(10, 120, 10, 30);
-ctx.fillRect(30, 120, 10, 30);
-ctx.fillRect(100, 120, 10, 30);
-ctx.fillRect(120, 120, 10, 30);
-ctx.fillRect(20, 150, 100, 10);
-ctx.fillRect(30, 160, 10, 20);
-ctx.fillRect(60, 170, 20, 10);
-ctx.fillRect(100, 160, 10, 20);
-ctx.fillRect(30, 180, 30, 10);
-ctx.fillRect(80, 180, 30, 10);
-ctx.fillStyle = "red";
-ctx.fillRect(60, 20, 50, 10);
-ctx.fillRect(50, 30, 30, 20);
-ctx.fillRect(100, 30, 20, 10);
-ctx.fillRect(110, 40, 10, 30);
-ctx.fillRect(100, 70, 20, 20);
-ctx.fillRect(90, 90, 20, 10);
-ctx.fillRect(50, 120, 10, 10);
-ctx.fillRect(70, 120, 10, 10);
-ctx.fillRect(90, 120, 10, 10);
-ctx.fillRect(20, 140, 10, 10);
-ctx.fillRect(50, 140, 40, 10);
-ctx.fillRect(110, 140, 10, 10);
-ctx.fillRect(40, 160, 10, 10);
-ctx.fillRect(70, 160, 10, 10);
-ctx.fillRect(90, 160, 10, 20);
-ctx.fillRect(50, 170, 10, 10);
-ctx.fillStyle = "#800000";
-ctx.fillRect(30, 20, 30, 10);
-ctx.fillRect(20, 30, 10, 10);
-ctx.fillRect(20, 70, 10, 20);
-ctx.fillRect(30, 90, 10, 10);
-ctx.fillRect(30, 110, 80, 10);
-ctx.fillRect(40, 120, 10, 10);
-ctx.fillRect(80, 120, 10, 10);
-ctx.fillRect(40, 130, 60, 10);
-ctx.fillRect(40, 140, 10, 10);
-ctx.fillRect(90, 140, 10, 10);
-ctx.fillRect(60, 160, 10, 10);
-ctx.fillRect(40, 170, 10, 10);
-ctx.fillRect(80, 170, 10, 10);
-ctx.fillStyle = "#cc9900";
-ctx.fillRect(30, 30, 20, 10);
-ctx.fillRect(20, 40, 10, 30);
-ctx.fillRect(30, 60, 10, 30);
-ctx.fillRect(40, 80, 50, 10);
-ctx.fillRect(40, 90, 10, 10);
-ctx.fillRect(80, 90, 10, 10);
-ctx.fillRect(20, 120, 10, 20);
-ctx.fillStyle = "yellow";
-ctx.fillRect(80, 30, 20, 20);
-ctx.fillRect(30, 40, 20, 10);
-ctx.fillRect(100, 40, 10, 30);
-ctx.fillRect(50, 50, 30, 10);
-ctx.fillRect(40, 60, 60, 20);
-ctx.fillRect(90, 80, 10, 10);
-ctx.fillRect(50, 90, 30, 10);
-ctx.fillRect(110, 120, 10, 20);
-ctx.fillRect(50, 160, 10, 10);
-ctx.fillRect(80, 160, 10, 10);
-ctx.fillStyle = "#3399ff"
-ctx.fillRect(60, 120, 10, 10);
+var canvas;
+var ctx;
+window.onload = init;
+
+function init(){
+    canvas = document.getElementById("my-canvas");
+    ctx = canvas.getContext('2d');
+    background();
+    
+}
+function background() {
+    ctx.beginPat
+    ctx.fillStyle = "black";
+    ctx.moveTo(995,595);
+    ctx.lineTo(993,3);
+    ctx.lineTo(3,3);
+    ctx.lineTo(3,597);
+
 }
