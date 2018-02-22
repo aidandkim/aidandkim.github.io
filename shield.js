d681c68b0837a8f3143beb572c51f23633693a57
aidandkim <aidandkim@gmail.com>
2018-02-21 19:37:09 -0800

    u suck so stop it get some help?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!?!??!?!?!?!?!?!?!??!?!?!?!?!?!?!

--- shield.js
+++ shield.js
@@ -5,5 +5,10 @@ window.onload = init;
 function init(){
     canvas = document.getElementById("my-canvas");
     ctx = canvas.getContext('2d');
+    rectangle();
 }
 
+function rectangle(){
+    ctx.fillStyle = "white"
+    ctx.fillRect(0,0,499,537);
+}
